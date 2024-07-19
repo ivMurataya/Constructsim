fe18ba93fc56e3f37883117b2332031a292770e9
Ivan Murataya <104288536+ivMurataya@users.noreply.github.com>
2024-07-18 21:33:53 -0400

    Create 06_LiveEdgeDetection.py

---
+++ OpenCV/OpenCv_Basics/06_LiveEdgeDetection.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+import rospy
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+import cv2
+import numpy as np
+
+
+class ShowingImage(object):
+
+    def __init__(self):
+    
+        self.image_sub = rospy.Subscriber("/camera/rgb/image_raw",Image,self.camera_callback)
+        self.bridge_object = CvBridge()
+
+    def camera_callback(self,data):
+        try:
+            # We select bgr8 because its the OpenCV encoding by default
+            cv_image = self.bridge_object.imgmsg_to_cv2(data, desired_encoding="bgr8")
+        except CvBridgeError as e:
+            print(e)
+        
+        img = cv_image
+        
+        #Convert the image to gray scale so the gradient is better visible
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        img = cv2.resize(img,(350,250))
+
+        #Apply the horizontal sobel operator with a kernel size of 3
+        sobelx = cv2.Sobel(img,cv2.CV_64F,1,0,ksize=3)
+
+        #Apply the vertical sobel operator with a kernel size of 3
+        sobely = cv2.Sobel(img,cv2.CV_64F,0,1,ksize=3)
+
+        cv2.imshow('Original',img)
+        cv2.imshow('sobelx',sobelx)
+        cv2.imshow('sobely',sobely)
+       
+        
+        cv2.waitKey(1)
+
+
+
+def main():
+    sobel_image_object = ShowingImage()
+    rospy.init_node('edge_detection_node', anonymous=True)
+    try:
+        rospy.spin()
+    except KeyboardInterrupt:
+        print("Shutting down")
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main()
