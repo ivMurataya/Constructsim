3aac933182a1ab7df609739595d66cb760268ad5
Ivan Murataya <104288536+ivMurataya@users.noreply.github.com>
2024-08-18 18:22:10 -0400

    Create __init__.py

---
+++ UnitTesting/BasicConcepts/__init__.py
@@ -0,0 +1,14 @@
+"""
+First of all, we are going to create a new package named robot_control.
+catkin_create_pkg robot_control rospy
+
+The next step will be to create another folder, inside the src folder, 
+named robot_control (yes, it has the same name as the original package).
+Now, inside this 2nd robot_control folder (robot_control/src/robot_control)
+
+
+The first thing we will do is to create an empty file, The __init__.py file makes Python treat directories containing it as modules.
+This will make it much more easier for us to import this file later in our test scripts.
+
+The second step will be to create a new file, named setup.py
+"""
