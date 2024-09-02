8d0cce6768b34bcd0975b127aa834f52a9706f8a
Ivan Murataya <104288536+ivMurataya@users.noreply.github.com>
2024-09-01 20:02:50 -0400

    Create 02_SimpleSubscriber.py

---
+++ 02_Ros2Basics/02_Ros2Topics/02_SimpleSubscriber.py
@@ -0,0 +1,74 @@
+import rclpy
+# import the ROS2 python libraries
+from rclpy.node import Node
+# import the LaserScan module from sensor_msgs interface
+from sensor_msgs.msg import LaserScan
+# import Quality of Service library, to set the correct profile and reliability to read sensor data.
+from rclpy.qos import ReliabilityPolicy, QoSProfile
+
+
+class SimpleSubscriber(Node):
+
+    def __init__(self):
+        # Here you have the class constructor
+        # call super() in the constructor to initialize the Node object
+        # the parameter you pass is the node name
+        super().__init__('simple_subscriber')
+        # create the subscriber object
+        # in this case, the subscriptor will be subscribed on /scan topic with a queue size of 10 messages.
+        # use the LaserScan module for /scan topic
+        # send the received info to the listener_callback method.
+        self.subscriber = self.create_subscription(
+            LaserScan,
+            '/scan',
+            self.listener_callback,
+            QoSProfile(depth=10, reliability=ReliabilityPolicy.RELIABLE))  # is the most used to read LaserScan data and some sensor data.
+
+    def listener_callback(self, msg):
+        # print the log info in the terminal
+        self.get_logger().info('I receive: "%s"' % str(msg))
+
+
+def main(args=None):
+    # initialize the ROS communication
+    rclpy.init(args=args)
+    # declare the node constructor
+    simple_subscriber = SimpleSubscriber()
+    # pause the program execution, waits for a request to kill the node (ctrl+c)
+    rclpy.spin(simple_subscriber)
+    # Explicity destroy the node
+    simple_subscriber.destroy_node()
+    # shutdown the ROS communication
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
+"""
+ros2 pkg create --build-type ament_python subscriber_pkg --dependencies rclpy std_msgs sensor_msgs
+
+Launch File:
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='subscriber_pkg',
+            executable='simple_subscriber',
+            output='screen'),
+    ])
+
+Setup.py
+import os
+from glob import glob
+ (os.path.join('share', package_name), glob('launch/*.launch.py'))
+  'simple_subscriber = subscriber_pkg.simple_subscriber:main'
+
+Complie:
+colcon build --packages-select subscriber_pkg
+
+
+"""
+
+
