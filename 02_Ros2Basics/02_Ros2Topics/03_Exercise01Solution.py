81e41eb9b9b82522547bdf65b3732d474d1b8efe
Ivan Murataya <104288536+ivMurataya@users.noreply.github.com>
2024-09-03 19:54:02 -0400

    Create 03_Exercise01Solution.py

---
+++ 02_Ros2Basics/02_Ros2Topics/03_Exercise01Solution.py
@@ -0,0 +1,80 @@
+import rclpy
+# import the ROS2 python libraries
+from rclpy.node import Node
+# import the Twist module from geometry_msgs interface
+from geometry_msgs.msg import Twist
+# import the LaserScan module from sensor_msgs interface
+from sensor_msgs.msg import LaserScan
+from rclpy.qos import ReliabilityPolicy, QoSProfile
+
+class Exercise31(Node):
+
+    def __init__(self):
+        # Here you have the class constructor
+        # call the class constructor
+        super().__init__('exercise31')
+        # create the publisher object
+        self.publisher_ = self.create_publisher(Twist, 'cmd_vel', 10)
+        # create the subscriber object
+        self.subscriber = self.create_subscription(LaserScan, '/scan', self.laser_callback, QoSProfile(depth=10, reliability=ReliabilityPolicy.RELIABLE))
+        # define the timer period for 0.5 seconds
+        self.timer_period = 0.5
+        # define the variable to save the received info
+        self.laser_forward = 0
+        # create a Twist message
+        self.cmd = Twist()
+        self.timer = self.create_timer(self.timer_period, self.motion)
+
+    def laser_callback(self,msg):
+        # Save the frontal laser scan info at 0°
+        self.laser_forward = msg.ranges[359] 
+        
+        
+    def motion(self):
+        # print the data
+        self.get_logger().info('I receive: "%s"' % str(self.laser_forward))
+        # Logic of move
+        if self.laser_forward > 5:
+            self.cmd.linear.x = 0.5
+            self.cmd.angular.z = 0.5
+        elif self.laser_forward < 5 and self.laser_forward >= 0.5:
+            self.cmd.linear.x = 0.2
+            self.cmd.angular.z = 0.0         
+        else:
+            self.cmd.linear.x = 0.0
+            self.cmd.angular.z = 0.0
+            
+        # Publishing the cmd_vel values to a Topic
+        self.publisher_.publish(self.cmd)
+
+
+            
+def main(args=None):
+    # initialize the ROS communication
+    rclpy.init(args=args)
+    # declare the node constructor
+    exercise31 = Exercise31()       
+    # pause the program execution, waits for a request to kill the node (ctrl+c)
+    rclpy.spin(exercise31)
+    # Explicity destroy the node
+    exercise31.destroy_node()
+    # shutdown the ROS communication
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
+
+
+"""
+Write a Publisher and Subscriber node:
+
+    Create a new package and call it exercise31_pkg with rclpy, std_msgs, sensor_msgs, and geometry_msgs as dependencies.
+    Use the previous publisher code as a template to create a new program that makes the robot turn left using Twist messages.
+    Get information from the laser and use the LaserScan messages to decide how to move.
+        Turn left until you get a value lower than 5m (from the laser direction in front of the robot). Then, move straight.
+        If the distance received is greater than 50cm, move in a straight line towards the wall (do not go too fast!).
+        If the distance received is less than 50cm, the robot stops.
+
+
+
+"""
