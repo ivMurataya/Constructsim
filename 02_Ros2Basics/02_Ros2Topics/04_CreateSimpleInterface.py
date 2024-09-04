99ce15db3e267e587e75434409c7b157c412654e
Ivan Murataya <104288536+ivMurataya@users.noreply.github.com>
2024-09-03 20:39:01 -0400

    Create 04_CreateSimpleInterface.py
    How to Create a Custom Interface

---
+++ 02_Ros2Basics/02_Ros2Topics/04_CreateSimpleInterface.py
@@ -0,0 +1,62 @@
+
+"""specify this is a CMake package with the --build-type flag set to ament_cmake"""
+
+# ros2 pkg create --build-type ament_cmake custom_interfaces --dependencies rclcpp std_msgs
+"""
+To create a new interface, you have to follow the next steps:
+
+    Create a directory named msg inside your package
+    Inside this directory, create a file named name_of_your_message.msg 
+    Modify the CMakeLists.txt file 
+    Modifypackage.xml file
+    Compile and source
+    Use in your node
+"""
+
+#------------  Age.msg ------------
+# int32 year
+# int32 month
+# int32 day
+
+#------------ CMakeLists.txt file ------------------
+"""
+# find dependencies
+find_package(ament_cmake REQUIRED)
+find_package(rclcpp REQUIRED)
+find_package(std_msgs REQUIRED)
+find_package(rosidl_default_generators REQUIRED)
+# find_package(<dependency> REQUIRED)
+
+if(BUILD_TESTING)
+endif()
+
+rosidl_generate_interfaces(${PROJECT_NAME}
+  "msg/Age.msg"
+)
+
+ament_package()
+
+"""
+
+# Add the following lines to the package.xml file:
+"""
+<build_depend>rosidl_default_generators</build_depend>
+<exec_depend>rosidl_default_runtime</exec_depend>
+<member_of_group>rosidl_interface_packages</member_of_group>
+"""
+
+# colcon build --packages-select custom_interfaces
+# source install/setup.bash
+# ros2 interface show custom_interfaces/msg/Age
+
+
+
+
+
+
+
+
+
+
+
+
