3bc4540a9e488849b0118b50fba957cb19cc9489
Ivan Murataya <104288536+ivMurataya@users.noreply.github.com>
2024-12-03 21:50:58 -0500

    Update 01_Simple_Publisher.py

--- 02_Ros2Basics/02_Ros2Topics/01_Simple_Publisher.py
+++ 02_Ros2Basics/02_Ros2Topics/01_Simple_Publisher.py
@@ -1,20 +1,23 @@
+#!/usr/bin/env python
+
 import rclpy
 # import the ROS2 python libraries
 from rclpy.node import Node
 # import the Twist interface from the geometry_msgs package
 from geometry_msgs.msg import Twist
+import signal
 
-class SimplePublisher(Node):
+class MoveRoverNode(Node):
 
     def __init__(self):
         # Here you have the class constructor
         # call super() in the constructor to initialize the Node object
         # the parameter you pass is the node name
-        super().__init__('simple_publisher')
+        super().__init__('move_rover_node')
         # create the publisher object
         # in this case, the publisher will publish on /cmd_vel Topic with a queue size of 10 messages.
         # use the Twist module for /cmd_vel Topic
-        self.publisher_ = self.create_publisher(Twist, 'cmd_vel', 10)
+        self.publisher_ = self.create_publisher(Twist, '/cmd_vel', 10)
         # define the timer period for 0.5 seconds
         timer_period = 0.5
         # create a timer sending two parameters:
@@ -34,22 +37,34 @@ class SimplePublisher(Node):
         self.publisher_.publish(msg)
         # Display the message on the console
         self.get_logger().info('Publishing: "%s"' % msg)
+
+    def stop_rover(self):
+        stopMsg = Twist()
+        self.publisher_.publish(stopMsg)
+        self.get_logger().info('Stoping the Robot')
+
             
 def main(args=None):
     # initialize the ROS communication
     rclpy.init(args=args)
     # declare the node constructor
-    simple_publisher = SimplePublisher()
+    simple_publisher = MoveRoverNode()
+    
     # pause the program execution, waits for a request to kill the node (ctrl+c)
+    def signal_handler(sig, frame):
+        simple_publisher.stop_rover()
+        simple_publisher.destroy_node()
+        rclpy.shutdown()
+
+    signal.signal(signal.SIGINT, signal_handler)
+    
+
     rclpy.spin(simple_publisher)
     # Explicity destroys the node
-    simple_publisher.destroy_node()
-    # shutdown the ROS communication
-    rclpy.shutdown()
+    
 
 if __name__ == '__main__':
     main()
-
 
 """
 ros2 pkg create --build-type ament_python publisher_pkg --dependencies rclpy std_msgs geometry_msgs
