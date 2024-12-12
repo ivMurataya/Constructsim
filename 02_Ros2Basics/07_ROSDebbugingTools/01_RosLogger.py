4b6cdea01d0ad75d414e6532f909d3f911dac9a5
Ivan Murataya <104288536+ivMurataya@users.noreply.github.com>
2024-12-12 11:02:26 -0500

    Update 01_RosLogger.py

--- 02_Ros2Basics/07_ROSDebbugingTools/01_RosLogger.py
+++ 02_Ros2Basics/07_ROSDebbugingTools/01_RosLogger.py
@@ -3,14 +3,14 @@ from rclpy.node import Node
 
 class LogDemo(Node):
     def __init__(self):
-        # call super() in the constructor in order to initialize the Node object
-        # the parameter we pass is the node name
         super().__init__('logger_example')
-        # Logger level configuration
-        rclpy.logging.set_logger_level('logger_example', rclpy.logging.LoggingSeverity.DEBUG)
-        # create a timer sending two parameters:
-        # - the duration between 2 callbacks (0.2 seeconds)
-        # - the timer function (timer_callback)
+        # Logger level configuration - Uncomment 1 line to set a specific log level
+        #rclpy.logging.set_logger_level('logger_example', rclpy.logging.LoggingSeverity.DEBUG)
+        #rclpy.logging.set_logger_level('logger_example', rclpy.logging.LoggingSeverity.INFO)
+        #rclpy.logging.set_logger_level('logger_example', rclpy.logging.LoggingSeverity.WARN)
+        #rclpy.logging.set_logger_level('logger_example', rclpy.logging.LoggingSeverity.ERROR)
+        #rclpy.logging.set_logger_level('logger_example', rclpy.logging.LoggingSeverity.FATAL)
+        
         self.create_timer(0.2, self.timer_callback)
         
     def timer_callback(self):
@@ -24,16 +24,11 @@ class LogDemo(Node):
         self.get_logger().error("Motor failure detected, unable to proceed with movement!")
         # prints a ROS2 log fatal
         self.get_logger().fatal("Critical error: Collision detected, emergency stop activated!")
-
-
+        
 def main(args=None):
-    # initialize the ROS communication
     rclpy.init(args=args)
-    # declare the node constructor
     log_demo = LogDemo()
-    # pause the program execution, waits for a request to kill the node (ctrl+c)
     rclpy.spin(log_demo)
-    # shutdown the ROS communication
     rclpy.shutdown()
 
 if __name__ == '__main__':
