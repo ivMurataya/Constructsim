aac464e0ad81356062d1866306de97a5818580ba
Ivan Murataya <104288536+ivMurataya@users.noreply.github.com>
2024-12-23 14:13:51 -0600

    Update 02_urdf_visualize_meshes_collisions_inertias.launch.py

--- 02_URDF_ROS2/03_URDF_for_Gazebo/02_urdf_visualize_meshes_collisions_inertias.launch.py
+++ 02_URDF_ROS2/03_URDF_for_Gazebo/02_urdf_visualize_meshes_collisions_inertias.launch.py
@@ -1,49 +1,49 @@
-#!/usr/bin/python3
-# -*- coding: utf-8 -*-
-import random
+import os
 
-from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-
+from launch.substitutions import Command
+from launch_ros.actions import Node
 
 # this is the function launch  system will look for
-
-
 def generate_launch_description():
 
+    ####### DATA INPUT ##########
+    urdf_file = 'box_bot_meshes_collisions_inertias.urdf'
+    #xacro_file = "box_bot.xacro"
+    package_description = "my_box_bot_description"
 
-    # Position and orientation
-    # [X, Y, Z]
-    position = [0.0, 0.0, 0.2]
-    # [Roll, Pitch, Yaw]
-    orientation = [0.0, 0.0, 0.0]
-    # Base Name or robot
-    robot_base_name = "box_bot"
+    ####### DATA INPUT END ##########
+    print("Fetching URDF ==>")
+    robot_desc_path = os.path.join(get_package_share_directory(package_description), "urdf", urdf_file)
 
+    # Robot State Publisher
 
-    entity_name = robot_base_name+"-"+str(int(random.random()*100000))
-
-    # Spawn ROBOT Set Gazebo
-    spawn_robot = Node(
-        package='gazebo_ros',
-        executable='spawn_entity.py',
-        name='spawn_entity',
-        output='screen',
-        arguments=['-entity',
-                   entity_name,
-                   '-x', str(position[0]), '-y', str(position[1]
-                                                     ), '-z', str(position[2]),
-                   '-R', str(orientation[0]), '-P', str(orientation[1]
-                                                        ), '-Y', str(orientation[2]),
-                   '-topic', '/robot_description'
-                   ]
+    robot_state_publisher_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='robot_state_publisher_node',
+        emulate_tty=True,
+        parameters=[{'use_sim_time': True, 'robot_description': Command(['xacro ', robot_desc_path])}],
+        output="screen"
     )
 
-    
+    # RVIZ Configuration
+    rviz_config_dir = os.path.join(get_package_share_directory(package_description), 'rviz', 'urdf_vis.rviz')
+
+
+    rviz_node = Node(
+            package='rviz2',
+            executable='rviz2',
+            output='screen',
+            name='rviz_node',
+            parameters=[{'use_sim_time': True}],
+            arguments=['-d', rviz_config_dir])
 
     # create and return launch description object
     return LaunchDescription(
-        [
-            spawn_robot,
+        [            
+            robot_state_publisher_node,
+            rviz_node
         ]
     )
