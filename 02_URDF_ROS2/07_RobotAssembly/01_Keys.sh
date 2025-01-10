579f6b6ecb40603ba7d0cb55f3d7d964762acb62
Ivan Murataya <104288536+ivMurataya@users.noreply.github.com>
2025-01-09 23:47:29 -0600

    Onshape API keys

--- 02_URDF_ROS2/07_RobotAssembly/01_Keys.sh
+++ 02_URDF_ROS2/07_RobotAssembly/01_Keys.sh
@@ -2,3 +2,10 @@
 export ONSHAPE_API=https://cad.onshape.com
 export ONSHAPE_ACCESS_KEY=Your_Access_Key
 export ONSHAPE_SECRET_KEY=Your_Secret_Key
+
+//cd ~/ros2_ws
+//source keys.sh
+//echo $ONSHAPE_API
+//echo $ONSHAPE_ACCESS_KEY
+//echo $ONSHAPE_SECRET_KEY
+
