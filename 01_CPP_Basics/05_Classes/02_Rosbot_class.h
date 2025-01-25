6e3e67114b07b4dc266ee18cb64c7a56fc21a3a0
Ivan Murataya <104288536+ivMurataya@users.noreply.github.com>
2025-01-24 21:25:36 -0600

    Rosbot_class.h

--- 01_CPP_Basics/05_Classes/02_Rosbot_class.h
+++ 01_CPP_Basics/05_Classes/02_Rosbot_class.h
@@ -1,3 +1,5 @@
+/*, we are including some ROS-related modules that we will use, 
+and also the modules to use lists and strings in your program, just like in previous lessons*/
 #ifndef ROSBOT_CLASS_H
 #define ROSBOT_CLASS_H
 #include "geometry_msgs/Twist.h"
@@ -9,8 +11,13 @@
 
 using namespace std;
 
+//we can see the definition of the class:
 class RosbotClass {
-private:
+/*The first variables we are going to declare are ROS related, and they communicate directly with the robot. 
+That's why we set them private, to keep better control of them.
+Also, the two methods that receive directly messages from the robot are set to private*/
+
+private: //Now we can see an access specifier:
   // Communicate with nodes
   ros::NodeHandle n;
   // Laser data
@@ -32,7 +39,11 @@ private:
   void odom_callback(const nav_msgs::Odometry::ConstPtr &odom_msg);
 
 public:
+/*Then, as we said in this chapter, comes the constructor of the class. 
+In this case we don't need to pass initial parameters to the RosbotClass, 
+so this constructor remains emtpy. It just initializes the class when an object is instanciated.*/
   RosbotClass();
+//See below the initialized functions that you have been using in order to move the robots around
   void move();
   void move_forward(int n_secs);
   void move_backwards(int n_secs);
