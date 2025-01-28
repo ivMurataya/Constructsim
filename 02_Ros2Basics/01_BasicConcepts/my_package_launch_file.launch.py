437b74400118c79876c4603c401c1a5a1b6a50f0
Ivan Murataya <104288536+ivMurataya@users.noreply.github.com>
2025-01-27 19:29:40 -0600

    Update my_package_launch_file.launch.py

--- 02_Ros2Basics/01_BasicConcepts/my_package_launch_file.launch.py
+++ 02_Ros2Basics/01_BasicConcepts/my_package_launch_file.launch.py
@@ -6,11 +6,12 @@ def generate_launch_description():
         Node(
             package='my_package',
             executable='simple_node',
-            output='screen'),
+            output='screen',
+            emulate_tty=True),
     ])
 """
 package=Package name: Name of the package that contains the code of the ROS2 program to execute
 executable=Name of python executable: Name of the Python executable file that you want to execute, we can add multiple entry points
 output=Type of output: Through which channel you will print the output of the program
-
+emulate_tty=True|False allows launch files to output colored log messages, Green=DEBUG, White=INFO, Orange=Warning, and Red=ERROR|Fatal.
 """
