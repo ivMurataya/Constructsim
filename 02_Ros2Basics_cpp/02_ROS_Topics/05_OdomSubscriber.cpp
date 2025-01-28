04b7dd25196e4df0445d9b8de6db55425bd39164
Ivan Murataya <104288536+ivMurataya@users.noreply.github.com>
2025-01-27 21:07:45 -0600

    OdomSubscriber.cpp

--- 02_Ros2Basics_cpp/02_ROS_Topics/05_OdomSubscriber.cpp
+++ 02_Ros2Basics_cpp/02_ROS_Topics/05_OdomSubscriber.cpp
@@ -1,37 +1,30 @@
-#include "nav_msgs/msg/detail/odometry__struct.hpp"
 #include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/int32.hpp"
 #include "nav_msgs/msg/odometry.hpp"
 using std::placeholders::_1;
 
-class SimpleSubscriber : public rclcpp::Node
+class OdomSubscriber : public rclcpp::Node
 {
 public:
-  // Initiate a Node called 'simple_subscriber'
-  SimpleSubscriber()
-  : Node("odom_subscriber")
+  OdomSubscriber()
+  : Node("simple_subscriber")
   {
-    // Create a Subscriber object that will listen to the /counter topic and will call the 'topic_callback' function       // each time it reads something from the topic
     subscription_ = this->create_subscription<nav_msgs::msg::Odometry>(
-      "odom", 10, std::bind(&SimpleSubscriber::topic_callback, this, _1));
+      "odom", 10, std::bind(&OdomSubscriber::odom_callback, this, _1));
   }
 
 private:
-  // Define a function called 'topic_callback' that receives a parameter named 'msg' 
-  void topic_callback(const nav_msgs::msg::Odometry::SharedPtr msg)
+  void odom_callback(const nav_msgs::msg::Odometry::SharedPtr msg)
   {
-    // Print the value 'data' inside the 'msg' parameter
-    double x_data = msg->pose.pose.position.x;
-    RCLCPP_INFO(this->get_logger(), "Robot X position: '%f'", x_data);
+    RCLCPP_INFO(this->get_logger(), "I heard: '%f'", msg->pose.pose.position.x);
   }
+
   rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr subscription_;
 };
 
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
-  // Create a loop that will keep the program in execution
-  rclcpp::spin(std::make_shared<SimpleSubscriber>());
+  rclcpp::spin(std::make_shared<OdomSubscriber>());
   rclcpp::shutdown();
   return 0;
 }
