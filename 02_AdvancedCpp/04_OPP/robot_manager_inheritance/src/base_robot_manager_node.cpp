67509a432919d544456b3345c25c1053afda1ed7
Ivan Murataya <104288536+ivMurataya@users.noreply.github.com>
2025-02-04 14:39:22 -0600

    Delete 02_AdvancedCpp/04_OPP/robot_manager_inheritance directory

--- 02_AdvancedCpp/04_OPP/robot_manager_inheritance/src/base_robot_manager_node.cpp
+++
@@ -1,9 +0,0 @@
-#include "robot_manager_inheritance/base_robot_manager.h"
-#include <ros/ros.h>
-
-int main(int argc, char **argv) {
-  ros::init(argc, argv, "robot_manager_using_inheritance");
-  ros::NodeHandle nh;
-  RobotManagerBase rmb = RobotManagerBase(&nh);
-  ros::spin();
-}
--- 02_AdvancedCpp/04_OPP/robot_manager_inheritance/src/mobile_robot_robot_manager_node.cpp
+++
@@ -1,9 +0,0 @@
-#include "robot_manager_inheritance/mobile_robot_robot_manager.h"
-#include <ros/ros.h>
-
-int main(int argc, char **argv) {
-  ros::init(argc, argv, "robot_manager");
-  ros::NodeHandle nh;
-  MobileRobotRobotManager mrrm = MobileRobotRobotManager(&nh);
-  ros::spin();
-}
