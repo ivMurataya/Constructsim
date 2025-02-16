3cb600e984815f13a7b488fae2e17734329b35e8
Ivan Murataya <104288536+ivMurataya@users.noreply.github.com>
2025-02-16 13:36:19 -0600

    Create Setup.py

---
+++ 09_IntermediateRos2/04_XMLandYAML/02_XML/Setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup
+import os
+from glob import glob
+
+package_name = 'launch_tests_pkg'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*')),
+        (os.path.join('share', package_name, 'rviz_config'), glob('rviz_config/*')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='user',
+    maintainer_email='user@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'move_robot_exe = launch_tests_pkg.move_robot:main',
+            'move_robot_with_arguments_exe = launch_tests_pkg.move_robot_with_arguments:main',
+            'move_robot_with_params_exe = launch_tests_pkg.move_robot_with_params:main',
+
+        ],
+    },
+)
+
+"""
+cd ~/ros2_ws/
+colcon build --symlink-install --packages-select launch_tests_pkg
+source install/setup.bash
+source /home/simulations/ros2_sims_ws/install/setup.bash
+ros2 launch launch_tests_pkg xml_main.launch.xml turning_speed:=5.0 forward_speed:=0.0
+
+
+"""
